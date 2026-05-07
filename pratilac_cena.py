d389efe8310b11dbdaccff77b28ebf99cd1a0053
vojinovic <vojinovic82@gmail.com>
2026-05-07 15:10:13 +0200

    Update pratilac_cena.py

--- pratilac_cena.py
+++ pratilac_cena.py
@@ -18,11 +18,10 @@ BAZA_FAJL = "cene_oglasa.json"
 PAUZA = 4
 
 HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
-    )
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Connection": "keep-alive",
 }
 
 
